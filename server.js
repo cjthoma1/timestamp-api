dd8441431c47f94f9fb1496cdc75bff4381fa679
Cray J Thomas Jr <cjthoma1@gmail.com>
2017-05-30 23:25:40 +0000

    temp conditional in place if parameter is not a number

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ var express = require("express");
 var app = express();
 var path = require('path');
 app.use(express.static(path.resolve(__dirname, 'client')))
-app.get('/hello', (req, res) =>{
-    res.send("Hello World!");
+app.get('/:number', (req, res) =>{
+    var date =  req.params.number
+    if(parseInt(date)) {
+        return res.send("Hello " + date +"!");
+    }
+    else{
+        return res.end('NULL');
+    }
+   
 })
+
+app.get("*", function(req, res) {
+  res.end("404");
+});
 
 app.listen('8080', () => {
     console.log('Example app listening on port 8080!')
